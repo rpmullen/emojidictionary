e8c5b0c621c17755cd6d680a11b658ef618b900f
Riley Mullen <rileymullen@Rileys-MBP.fios-router.home>
2017-06-28 18:20:05 -0400

    Final Dictionary

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView: UITableView!
     
     
-    var emojis = ["😊","✌🏿","🤘🏿","💩","💪","😇","🎅🏿","👀","🐼","🦁","🙈","🙊","♻️","⚜️","☘️","🥑","🏒","🇺🇸"]
+    var emojis = ["😊","👍🏿","🤘🏿","💩","💪","😇","🎅🏿","👀","🐼","🦁","🙈","🙊","♻️","⚜️","☘️","🥑","🏒","🇺🇸"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +34,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
         
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let defVC = segue.destination as! DefinitionViewController
+        defVC.emoji = sender as! String
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
